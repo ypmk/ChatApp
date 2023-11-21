c63c22585f357f7890821e90644ed22071de29d3
ypmk <kopylovaksusha123@gmail.com>
2023-11-21 12:55:39 +0300

    Создан файл signin.php

--- signin.php
+++ signin.php
@@ -6,7 +6,7 @@
     <meta http-equiv="XA-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
-    <linl rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
@@ -17,9 +17,22 @@
                 <h2>Sign In</h2>
                 <p>Login to MyChat</p>
             </div>
-
-
-
-    
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" placeholder="example@gmail.com" 
+                autocomplete="off" required>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" name="pass" placeholder="Password" 
+                autocomplete="off" required>
+            </div>
+            <div class="small">Forgot password?<a href="forgot_pass.php">Click Here</a></div><br>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_in">Sign in</button>
+            </div>
+            <!--<?php //include("signin_user.php"); ?> -->
+        </form>
+        <div class="text-center-small" style="color:#67428B;">Don't have an account?<a href="signup.php">Create one</a></div>
 </body>
 </html>
