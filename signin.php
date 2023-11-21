e62934ba1bbb1942509e46c1e14f22145a5eafb7
ypmk <kopylovaksusha123@gmail.com>
2023-11-21 12:23:10 +0300

    first commit

---
+++ signin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login to your account</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="XA-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+    <linl rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="signin-form">
+        <form action="" method="post">
+            <div class="form-header">
+                <h2>Sign In</h2>
+                <p>Login to MyChat</p>
+            </div>
+
+
+
+    
+</body>
+</html>
